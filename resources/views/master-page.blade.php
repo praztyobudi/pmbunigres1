8e01e60084af8e2fb4de37707c99108e3f06590c
praztyobudi <praztyob@gmail.com>
2021-04-26 16:26:58 +0700

    aak

--- resources/views/master-page.blade.php
+++ resources/views/master-page.blade.php
@@ -24,7 +24,7 @@
     <div class="main-container">
         <a class="navbar-brand" href="{{ route('homepage') }}">
             <img class="logo-brand" src="{{ asset('unigres/images/logo.png') }}">
-            <p>PMB.<span>Unigres</span></p>
+            <p>AAK<span>Unigres</span></p>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
